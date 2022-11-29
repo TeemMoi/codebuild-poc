c176bf2e402a1cdb5c0f4a3ea275020ba6ceb37a
TeemMoi <teemu.moisanen@northcode.fi>
2022-11-29 13:35:47 +0200

    added failing test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('../src/sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(4);
+});
