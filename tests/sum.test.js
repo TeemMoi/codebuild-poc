c8b3547c77b5ae0c531408722b1c95d599fa60e0
TeemMoi <teemu.moisanen@northcode.fi>
2022-11-30 09:29:30 +0200

    trying webhook

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(4);
 });
+
+test('adds 2 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(4);
+});
